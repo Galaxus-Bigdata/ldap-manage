0bd468512ccf8fcadc519a2f4f04a1ff55ea3daf
Galaxus Tech Solutions <nooralam117@gmail.com>
2024-04-23 15:47:27 +0530

    Update avroIndexToSolr.java

--- avroIndexToSolr.java
+++ avroIndexToSolr.java
@@ -1,6 +1,4 @@
 import org.apache.avro.generic.GenericRecord;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -30,21 +28,29 @@ public class IndexAvroDataToSolr {
         try {
             // Read JSON file to extract column names to index
             String jsonFilePath = "path/to/your/json/file";
-            List<String> columnsToIndex = getColumnNames(jsonFilePath);
+            List<String> fieldNames = getFieldNames(jsonFilePath);
 
             // Load Avro data
             Dataset<Row> avroData = spark.read().format("avro").load("path/to/your/avro/file");
 
-            // Select required columns
-            Dataset<Row> selectedData = avroData.select(columnsToIndex.stream().map(col -> functions.col(col)).toArray(Column[]::new));
-
             // Convert DataFrame to SolrInputDocument and index into Solr
-            selectedData.foreach(row -> {
-                SolrInputDocument doc = new SolrInputDocument();
-                for (String columnName : columnsToIndex) {
-                    doc.addField(columnName, row.getAs(columnName));
+            avroData.foreachPartition(rows -> {
+                try (SolrClient solrClient = new HttpSolrClient.Builder("http://localhost:8983/solr").build()) {
+                    rows.forEachRemaining(row -> {
+                        SolrInputDocument doc = new SolrInputDocument();
+                        for (int i = 0; i < fieldNames.size(); i++) {
+                            doc.addField(fieldNames.get(i), row.getAs(i).toString());
+                        }
+                        try {
+                            solrClient.add(doc);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    solrClient.commit();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                indexToSolr(doc); // Method to index document into Solr
             });
 
         } catch (IOException e) {
@@ -54,42 +60,19 @@ public class IndexAvroDataToSolr {
         spark.stop();
     }
 
-    private static List<String> getColumnNames(String jsonFilePath) throws IOException {
-        List<String> columnNames = new ArrayList<>();
+    private static List<String> getFieldNames(String jsonFilePath) throws IOException {
+        List<String> fieldNames = new ArrayList<>();
         try (Reader reader = new FileReader(jsonFilePath)) {
             // Parse JSON file
             JsonObject jsonObject = new Gson().fromJson(reader, JsonObject.class);
-            // Extract column names array
-            JsonArray columnsArray = jsonObject.getAsJsonArray("columns");
-            if (columnsArray != null) {
-                for (int i = 0; i < columnsArray.size(); i++) {
-                    columnNames.add(columnsArray.get(i).getAsString());
+            // Extract field names array
+            JsonArray fieldsArray = jsonObject.getAsJsonArray("fields");
+            if (fieldsArray != null) {
+                for (int i = 0; i < fieldsArray.size(); i++) {
+                    fieldNames.add(fieldsArray.get(i).getAsString());
                 }
             }
         }
-        return columnNames;
-    }
-
-    private static void indexToSolr(SolrInputDocument doc) {
-        // Initialize SolrClient with HTTPS URL and Kerberos Authentication
-        String solrUrl = "https://your-solr-host:8983/solr";
-        Configuration conf = new Configuration();
-        conf.set("hadoop.security.authentication", "kerberos");
-        UserGroupInformation.setConfiguration(conf);
-        try {
-            UserGroupInformation.loginUserFromKeytab("your_principal@YOUR_REALM", "/path/to/your/keytab");
-            SolrClient solrClient = new HttpSolrClient.Builder(solrUrl)
-                    .withHttpClient(org.apache.solr.client.solrj.impl.HttpClientUtil.createClient(null))
-                    .withKerberosKeytab("/path/to/your/keytab")
-                    .withKerberosPrincipal("your_principal@YOUR_REALM")
-                    .build();
-            // Index doc into Solr
-            solrClient.add(doc);
-            // Commit changes to Solr
-            solrClient.commit();
-            solrClient.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        return fieldNames;
     }
 }
